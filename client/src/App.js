8c7635b1e1aa1944516f530246a1485bc12f39e0
Jacob Kriese <jkriese12@gmail.com>
2021-04-12 21:21:48 -0700

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Photoscribe... Coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
