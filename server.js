aee2df1eda5dc3e7b42fae0f72d77616d6fb3a7f
Jacob Kriese <jkriese12@gmail.com>
2021-04-14 21:19:06 -0700

    New dependicies, backend framework

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(cors());
+const CONNECTION_URL =
+  "mongodb+srv://jkriese12:H0ldth3Door@cluster0.spksx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
-
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// // Add routes, both API and view
+// app.use(routes);
+require("./routes/routes.js")(app);
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+    )
+  )
+  .catch((error) => console.log(error.message));
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+mongoose.set("useFindAndModify", false);
