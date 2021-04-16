85406507ded1e1969387fe899fa06ffd927d5ce5
Tiffany Crockett <crockett.tiff@gmail.com>
2021-04-16 12:04:28 -0700

    comp pages fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from "react"; 
+// eslint-disable-next-line
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
 
 function App() {
   return (
     <div>
-      <h1>Photoscribe... Coming soon</h1>
+      
+
     </div>
   );
 }
