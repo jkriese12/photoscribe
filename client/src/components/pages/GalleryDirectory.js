ae593cb0eb0dc369c4fc517fd18965f1adb9ce4b
Tiffany Crockett <crockett.tiff@gmail.com>
2021-04-17 01:41:56 -0700

    gallerydir, gallerypg

--- client/src/components/pages/GalleryDirectory.js
+++ client/src/components/pages/GalleryDirectory.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const GalleryDirectory = () => {
+const GalleryDirectory = () => { 
     return (
-        <div>
-            
+        <div className="py-5 bg-light">
+            <Container>
+                <Row>
+                    {gallery.map((item, key) => {
+                        return (
+                            <Col md="2" key={key}>
+                                <Card className="mb-4 box-shadow">
+                                    <Card.Img
+                                        top
+                                        width="100%"
+                                        src={item.src}
+                                        alt={item.altText}
+                                    />
+                                    
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
         </div>
-    )
-}
+    );
+};
+        
 
-export default GalleryDirectory
+export default GalleryDirectory;
