228240fb7fc2d04951fd200a061209ab813fd891
Jacob Kriese <jkriese12@gmail.com>
2021-04-17 11:40:54 -0700

    Get all posts, and create a post back end to front end code added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import getPosts from "./actions/posts";
+import { getPosts } from "./actions/posts";
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,3 +2,4 @@ import axios from "axios";
 // Defining URL to call from API and exporting function to grab from database
 const url = "http://localhost:3001/api/photos";
 export const getPhotos = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
