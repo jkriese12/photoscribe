45b34c05621dd0a56cc911620abed8f38d13ab49
jkriese12 <jkriese12@gmail.com>
2021-04-19 20:03:34 -0700

    Moved all backend testing to own folder

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,10 @@ import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Scribe from "./pages/Scribe";
-import Testpost from "./pages/Testpost";
 
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
-import Testget from "./pages/Testget";
+
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -23,8 +22,6 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
             <Route path="/scribe" component={Scribe} />
-            <Route path="/test" component={Testpost} />
-            <Route path="/testget" component={Testget} />
           </>
         </Switch>
       </Router>
