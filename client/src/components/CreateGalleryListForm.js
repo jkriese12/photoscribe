9765c577f77cd009b9e747e846e4a37aecf79ae8
Tiffany Crockett <crockett.tiff@gmail.com>
2021-04-29 05:27:44 -0700

    corrected createdGalleryListForm and GalleryTemplate issues - again - hope github gits it! ;-)

--- client/src/components/CreateGalleryListForm.js
+++ client/src/components/CreateGalleryListForm.js
@@ -1,45 +1,6 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { createPost } from "../actions/posts";
-
-
-import "./styles/CreateGalleryListForm.css";
-
-const CreateGalleryListForm = (props) => { 
-
-const [galleryName, setGalleryName] = useState({
-     name: "",
- }); 
-
-// const dispatch = useDispatch();
-// const submitHandler = (e) => {
-//     e.preventDefault();
-
-//     dispatch(createPost(details));
-//   };
-
-    return (  
-
-        <form className="create-name">
-            <div className="form-group">
-                <label htmlFor="gallery-name">Create a Gallery:</label><br/>
-                <input
-                    type="name"
-                    placeholder="Name"
-                    className="form-group"
-                    // onSubmit={(e) => setDetails({  name : e.target.value })}
-                    // value={details.name}
-                 /> 
-            </div> 
-            <button type="submit" onClick={props.handleFormSubmit} className="btn btn-sm btn-secondary">Create</button>
-        </form> 
-                     
-    );
-}
-
 import { useSelector } from "react-redux";
-
-import "./CreateGalleryLIstForm.css";
+import "./styles/CreateGalleryListForm.css";
 
 const CreateGalleryListForm = (props) => {
   const [galleryName, setGalleryName] = useState({
