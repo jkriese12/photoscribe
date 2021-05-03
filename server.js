08fa6dcea3b2bfa775cec6fed980731c362fe3e6
jkriese12 <jkriese12@gmail.com>
2021-05-03 15:28:49 -0700

    Server file size update

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 // Middleware denfined
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
