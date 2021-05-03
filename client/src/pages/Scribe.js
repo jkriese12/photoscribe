62cdabc6d3d12036b4e90722f5695fad4fff0eb7
jkriese12 <jkriese12@gmail.com>
2021-05-03 14:54:16 -0700

    changed photo source

--- client/src/pages/Scribe.js
+++ client/src/pages/Scribe.js
@@ -12,7 +12,7 @@ import GalleryCardText from "../components/GalleryCardText";
 import SelectedGallery from "../components/SelectedGallery";
 import ScribeCardPreview from "../components/ScribeCardPreview";
 import Logout from "../components/Logout";
-// import photo from "../components/Images/undraw_cms_re_asu0.png";
+import photo from "../components/Images/undraw_cms_re_asu0.png";
 import "./styles/Scribe.css";
 
 const Scribe = () => {
@@ -26,9 +26,8 @@ const Scribe = () => {
   });
   const [newGallery, setNewGallery] = useState("");
   // Default picture and state for cropped image that will be displayed on the page
-  const defaultSrc =
-    "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
-  const [image, setImage] = useState(defaultSrc);
+
+  const [image, setImage] = useState(photo);
   const [cropData, setCropData] = useState("#");
   const [cropper, setCropper] = useState("");
   const img = document.getElementById("test");
@@ -41,7 +40,7 @@ const Scribe = () => {
       photoLocation: "",
       synopsis: "",
     });
-    setImage(defaultSrc);
+    setImage(photo);
     setCropData("#");
     setNewGallery("");
   };
