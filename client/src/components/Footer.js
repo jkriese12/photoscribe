720c30708e8c00817e99b787f04f0e91f59d4cf4
jkriese12 <jkriese12@gmail.com>
2021-05-03 06:34:14 -0700

    env upate

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="photoscribe-footer">
-            <div className="footer-text">
-                <p>PhotoScribe 2021</p>   
-            </div> 
-        </div>
-    )
-}
+  return (
+    <div className="photoscribe-footer">
+      <div className="footer-text">
+        <p>PhotoScribe 2021</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
