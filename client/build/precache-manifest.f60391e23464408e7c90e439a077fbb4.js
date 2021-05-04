340d1ea302a1276df14293858c1b9b2c7e777678
jkriese12 <jkriese12@gmail.com>
2021-05-03 17:23:23 -0700

    .

---
+++ client/build/precache-manifest.f60391e23464408e7c90e439a077fbb4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1164e9397e674ac2bcee8378aada6cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9b4ce5e594543eca13",
+    "url": "/static/css/2.3c4d953a.chunk.css"
+  },
+  {
+    "revision": "237d82468e5b9e829e7d",
+    "url": "/static/css/main.f02ed595.chunk.css"
+  },
+  {
+    "revision": "bf9b4ce5e594543eca13",
+    "url": "/static/js/2.5a57f433.chunk.js"
+  },
+  {
+    "revision": "6fd1c03969c168eebe39bf9afd8bc424",
+    "url": "/static/js/2.5a57f433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237d82468e5b9e829e7d",
+    "url": "/static/js/main.a135305b.chunk.js"
+  },
+  {
+    "revision": "c802eba58b7fdc287271",
+    "url": "/static/js/runtime-main.a56ad2c1.js"
+  },
+  {
+    "revision": "1c046500f176e62c85bebc00019164a1",
+    "url": "/static/media/snap_the_moment.1c046500.svg"
+  },
+  {
+    "revision": "1587eb410a70061524a6c387829a1bd7",
+    "url": "/static/media/three.1587eb41.png"
+  },
+  {
+    "revision": "2a9cd3667c89941cad79462b8480dffd",
+    "url": "/static/media/try-it-now.2a9cd366.jpg"
+  },
+  {
+    "revision": "91829944877137e8cbf11744218928f5",
+    "url": "/static/media/two.91829944.png"
+  }
+]);
